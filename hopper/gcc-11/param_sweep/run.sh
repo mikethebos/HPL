9e88df13f9ee3bde95c6b5ae6eda6525905fde12
Louis <ljencka@unm.edu>
2024-04-17 09:39:03 -0600

    Minimize amount of requested resources

--- hopper/gcc-11/param_sweep/run.sh
+++ hopper/gcc-11/param_sweep/run.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-
-for NNODES in 1 2 3; do
+for NNODES in 1 2 8; do
     for NTASKS in 1 8 16; do
-        for CPUS in 1; do
-            count=$(wc -l < HPL_params.csv)
+        for CPUS in 1 2 4; do
             sbatch \
                 --job-name HPL_ParamSweep_${NNODES}_${NTASKS} \
                 --partition general \
-                --time 00:10:00 \
+                --time 00:00:15 \
                 --nodes $NNODES \
                 --ntasks-per-node $NTASKS \
                 --cpus-per-task $CPUS \
-                --mem 91GB \
-                --array "1-$count" \
+                --mem-per-cpu 1200M \
+                --array "1-$(wc -l < HPL_params.csv)" \
                 parameter_sweep_array.slurm
         done
     done
